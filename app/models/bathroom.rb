cceba4a2f344ca69c40175e049ac5ca8ca1fe0aa
github username <github email address>
2020-05-07 13:44:35 -0400

    Updated Log In and Log Out pages and included a price calculator for bathrooms

--- app/models/bathroom.rb
+++ app/models/bathroom.rb
@@ -3,4 +3,13 @@ class Bathroom < ApplicationRecord
     has_many :houses, through: :housebathrooms
     belongs_to :room_style
     belongs_to :user, optional: true
+
+    def bathroom_cost
+        if self.half?
+            half_cost = 0.75
+        else
+            half_cost = 1.25
+        end
+    bathroom_cost = (self.size * self.room_style.cost * 10 * half_cost).round(2)
+    end
 end
